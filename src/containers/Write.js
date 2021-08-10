df092a5bb3efbcd9e5076c4ccb6b413d2746b016
Patricio Vargas <pato@onesignal.com>
2021-08-10 01:02:09 -0400

    writting

--- src/containers/Write.js
+++ src/containers/Write.js
@@ -1,6 +1,29 @@
 import React from 'react';
 
 const Write = () => {
+
+    const onWrite = async() => {
+        const abortController = new AbortController();
+        abortController.signal.onabort = event => {
+            alert('STOP');
+        };
+        try {
+            const ndef = new window.NDEFReader();
+            await ndef.write(
+                {
+                    records: [{ recordType: "text", data: "17" }]
+                },
+                {signal: abortController.signal}
+            );
+            alert(`${16} saved!`);
+            abortController.abort();
+        } catch (error) {
+            console.log(error);
+            abortController.abort();
+        }
+    };
+
+    onWrite();
     return (
       "Write"
     );
