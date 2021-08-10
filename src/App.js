3c1846d9858810b410af39bcf8ca08b4e1400726
Patricio Vargas <pato@onesignal.com>
2021-08-09 20:09:51 -0400

    hide/show scan and write

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/Button/Button';
 import Scan from './containers/Scan';
 import Write from './containers/Write';
+import { useState } from 'react';
 
 function App() {
+  const [isScan, setScan] = useState(false);
+  const [isWrite, setWrite] = useState(false);
+
   return (
     <div className="App">
-      <Button name="Scan"/>
-      <Button name="Write"/>
-      <Scan/>
-      <Write/>
+      <img src={logo} className="App-logo" alt="logo" />
+      <button onClick={()=>setScan(!isScan)} className="">Scan</button>
+      <button onClick={()=>setWrite(!isWrite)} className="">Write</button>
+      {isScan && <Scan/>}
+      {isWrite && <Write/>}
+ 
     </div>
   );
 }
